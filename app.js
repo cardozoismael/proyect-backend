43b86b47fe6853e74fc7274e526c694cd3f1b644
ismael cardozo <cardozoismael753@gmal.com>
2025-10-30 14:18:21 -0300

    segunda modificacion del proyecto backend

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-jejeje
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req,res)=>{
+    res.send('bien hecho, el servidor funciona correctamente');
+})
+
+app.get('/api/data',(req,res)=>{
+    res.json({message: 'esta es una ruta de API que devuelve datos en formato JSON'});
+})
+
+app.listen(PORT, ()=>{
+    console.log(`server esta corriendo en el puerto ${PORT}`)
+})
